ad5625c2bcf4064fa3f2af5ee0abbdb8d5151a3b
AsteriUwU <asteri_uwu@outlook.com>
2023-05-18 01:07:37 -0600

    Upload

---
+++ script/flowerCards.js
@@ -0,0 +1,63 @@
+function openAbies(){
+  document.getElementById("abies").style.width = "100%";
+}
+  
+function closeAbies(){
+  document.getElementById("abies").style.width = "0%";
+}
+
+function openAbutilon(){
+  document.getElementById("abutilon").style.width = "100%";
+}
+  
+function closeAbutilon(){
+  document.getElementById("abutilon").style.width = "0%";
+}
+
+function openBambusaVulgaris(){
+  document.getElementById("bambusaVulgaris").style.width = "100%";
+}
+  
+function closeBambusaVulgaris(){
+  document.getElementById("bambusaVulgaris").style.width = "0%";
+}
+
+function openBanksiaLaricina(){
+  document.getElementById("banksiaLaricina").style.width = "100%";
+}
+  
+function closeBanksiaLaricina(){
+  document.getElementById("banksiaLaricina").style.width = "0%";
+}
+
+function openCactaceae(){
+  document.getElementById("cactaceae").style.width = "100%";
+}
+  
+function closeCactaceae(){
+  document.getElementById("cactaceae").style.width = "0%";
+}
+
+function openCaladium(){
+  document.getElementById("caladium").style.width = "100%";
+}
+  
+function closeCaladium(){
+  document.getElementById("caladium").style.width = "0%";
+}
+
+function openDahlia(){
+  document.getElementById("dahlia").style.width = "100%";
+}
+  
+function closeDahlia(){
+  document.getElementById("dahlia").style.width = "0%";
+}
+
+function openDaphne(){
+  document.getElementById("daphne").style.width = "100%";
+}
+  
+function closeDaphne(){
+  document.getElementById("daphne").style.width = "0%";
+}
